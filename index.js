283b7932f1b71f3fb9d51ac006c843d80e593bca
mjimenezni <mjimenezni@educa.jcyl.es>
2020-04-17 20:26:39 +0200

    commit 6

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-const name = 'world';
-console.log(`Hello ${name}`);
+const name = 'Bienvenidos';
+console.log(`${name}`);
+
+//Añado un evento al botón para que al hacer click muestre la ventana modal utilizando Bootstrap 4
+document
+  .querySelector('#myBtn')
+  .addEventListener('click', () => document.querySelector('#myModal').modal());
